09d4024ee0882b1c6a6cc8987ba45a7682d970b4
tochkov <philip.tochkov@gmail.com>
2024-12-15 00:06:54 +0200

    Initial DI setup

--- app/src/main/java/com/topdownedge/wealthgrid/MainActivity.kt
+++ app/src/main/java/com/topdownedge/wealthgrid/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.topdownedge.presentation.navigation.WealthGridApp
+import com.topdownedge.presentation.WealthGridApp
 import com.topdownedge.wealthgrid.ui.theme.WealthGridTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
